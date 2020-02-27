b2ad7083c9826afa6bb0a68086701c38b4d9055c
Enoque Almeida De Souza <e.almeida.de.souza@avanade.com>
2020-02-27 17:23:16 -0300

    Commit inicial

---
+++ src/main/java/com/cursos/apirest/ApiRestComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cursos.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestComSpringBootApplication.class, args);
+	}
+
+}
